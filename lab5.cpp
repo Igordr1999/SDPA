c23e0c9593854c72ff14a0df8862972e182ee45d
Igordr1999 <Igordr1999@yandex.ru>
2018-10-28 12:34:19 +0300

    Create lab5

---
+++ lab5.cpp
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <map>
+using namespace std;
+
+int main(){
+	char str[255];
+	cin.getline(str, 255);
+	char * speech = strtok(str, " ,.-"); // the second parameter is the separator
+	map<string, int> words;
+
+
+	while (speech != NULL){	// while there are lexemes
+		words[speech]++;
+		speech = strtok(NULL, " ,.-?!");
+	}
+	cout << endl<<"Word\tTotal"<<endl;
+	for (auto it = words.begin(); it != words.end(); it++){
+		cout << (it->first)<<"\t";
+		cout << (it->second) << endl;
+	}
+	return 0;
+}
